618dbb4e6ab7b34bff084c3129ab0233cbb257c7
Aleksandr Nesterovich <nesterovich.aleksandr.a@gmail.com>
2019-01-11 20:50:52 +0300

    added logger in dao

--- src/main/java/by/pvt/dao/impl/BaseDaoImpl.java
+++ src/main/java/by/pvt/dao/impl/BaseDaoImpl.java
@@ -8,9 +8,12 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.logging.Logger;
 
 @Transactional
 public abstract class BaseDaoImpl<T, PK extends Serializable> implements BaseDao<T, PK> {
+
+    private static Logger log = Logger.getLogger(BaseDaoImpl.class.getName());
 
     @Autowired
     SessionFactory sessionFactory;
@@ -24,28 +27,34 @@ public abstract class BaseDaoImpl<T, PK extends Serializable> implements BaseDao
     public void create(T entity) {
         Session session = openSession();
         session.save(entity);
+        log.info(aClass + " create(): \n" + entity);
     }
 
     public T read(Serializable id) {
         Session session = openSession();
         T entity = session.get(aClass, id);
+        log.info(aClass + " read(): \n" + entity);
         return entity;
     }
 
     public void update(T entity) {
         Session session = openSession();
         session.update(entity);
+        log.info(aClass + " update(): \n" + entity);
     }
 
     public void delete(Serializable id) {
         Session session = openSession();
         T entity = session.get(aClass, id);
         session.delete(entity);
+        log.info(aClass + " delete(): id=" + id);
     }
 
     public List<T> getAll() {
         String nameClass = (aClass.getSimpleName());
-        return openSession().createQuery("from " + nameClass).list();
+        List list = openSession().createQuery("from " + nameClass).list();
+        log.info(aClass + " getAll(): \n" + list);
+        return list;
     }
 
     Session openSession() {

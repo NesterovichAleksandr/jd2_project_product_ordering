70478da78513dd197f42431d60a0a3080819d3eb
Aleksandr Nesterovich <nesterovich.aleksandr.a@gmail.com>
2019-01-11 20:20:27 +0300

    added logger in service

--- src/main/java/by/pvt/service/impl/BaseServiceImpl.java
+++ src/main/java/by/pvt/service/impl/BaseServiceImpl.java
@@ -10,10 +10,13 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.logging.Logger;
 
 @Service
 @Transactional
 public abstract class BaseServiceImpl<T, PK extends Serializable> implements BaseService<T, PK> {
+
+    private static Logger log = Logger.getLogger(BaseServiceImpl.class.getName());
 
     private BaseDao<T, PK> dao;
 
@@ -24,26 +27,31 @@ public abstract class BaseServiceImpl<T, PK extends Serializable> implements Bas
 
     @Transactional(propagation = Propagation.REQUIRES_NEW, isolation = Isolation.REPEATABLE_READ)
     public void create(T entity) {
+        log.info("create(): " + entity);
         dao.create(entity);
     }
 
     @Transactional(propagation = Propagation.REQUIRES_NEW, readOnly = true)
     public T read(PK id) {
+        log.info("read(): id=" + id + "\t: " + getClass());
         return dao.read(id);
     }
 
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void update(T entity) {
+        log.info("update(): " + entity);
         dao.update(entity);
     }
 
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void delete(PK id) {
+        log.info("delete(): id=" + id + "\t: " + getClass());
         dao.delete(id);
     }
 
-    @Transactional(propagation =  Propagation.REQUIRED, readOnly = true)
+    @Transactional(propagation = Propagation.REQUIRED, readOnly = true)
     public List<T> getAll() {
+        log.info("getAll(): " + dao.getAll() + "\t: " + getClass());
         return dao.getAll();
     }
 }

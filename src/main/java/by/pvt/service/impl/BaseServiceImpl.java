adb8687cb61e78bb8f41f143d079cc7fe492c3dd
Aleksandr Nesterovich <nesterovich.aleksandr.a@gmail.com>
2019-01-13 09:58:49 +0300

    fix transaction

--- src/main/java/by/pvt/service/impl/BaseServiceImpl.java
+++ src/main/java/by/pvt/service/impl/BaseServiceImpl.java
@@ -25,32 +25,32 @@ public abstract class BaseServiceImpl<T, PK extends Serializable> implements Bas
         this.dao = dao;
     }
 
-    @Transactional(propagation = Propagation.REQUIRES_NEW, isolation = Isolation.REPEATABLE_READ)
+    //@Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.REPEATABLE_READ)
     public void create(T entity) {
         log.info(getClass() + " create(): \n" + entity);
         dao.create(entity);
     }
 
-    @Transactional(propagation = Propagation.REQUIRES_NEW, readOnly = true)
+    //@Transactional(propagation = Propagation.REQUIRED, readOnly = true)
     public T read(PK id) {
         T entity = dao.read(id);
         log.info(getClass() + " read(): " + "\n" + entity);
         return entity;
     }
 
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    //@Transactional(propagation = Propagation.REQUIRED)
     public void update(T entity) {
         log.info(getClass() + " update(): \n" + entity);
         dao.update(entity);
     }
 
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    //@Transactional(propagation = Propagation.REQUIRED)
     public void delete(PK id) {
         log.info(getClass() + " delete(): id=" + id);
         dao.delete(id);
     }
 
-    @Transactional(propagation = Propagation.REQUIRED, readOnly = true)
+    //@Transactional(propagation = Propagation.REQUIRED, readOnly = true)
     public List<T> getAll() {
         List<T> list = dao.getAll();
         log.info(getClass() + " getAll(): \n" + list);

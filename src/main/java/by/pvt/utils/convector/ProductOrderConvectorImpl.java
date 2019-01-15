afbec44986c542f9355ca12427fee52771708c4d
Aleksandr Nesterovich <nesterovich.aleksandr.a@gmail.com>
2019-01-15 16:06:05 +0300

    fix ProductOrderConvectorImpl added check on null

--- src/main/java/by/pvt/utils/convector/ProductOrderConvectorImpl.java
+++ src/main/java/by/pvt/utils/convector/ProductOrderConvectorImpl.java
@@ -6,37 +6,73 @@ import java.util.List;
 
 public class ProductOrderConvectorImpl {
 
-    public ProductOrder updateConvectorToEntity(ProductOrder entity, ProductOrder dto){
+    public ProductOrder updateConvectorToEntity(ProductOrder entity, ProductOrder dto) {
 
-        entity.setHref(dto.getHref());
-        entity.setExternalId(dto.getExternalId());
-        entity.setPriority(dto.getPriority());
-        entity.setDescription(dto.getDescription());
-        entity.setCategory(dto.getCategory());
-        entity.setState(dto.getState());
-        entity.setOrderDate(dto.getOrderDate());
-        entity.setCompletionDate(dto.getCompletionDate());
-        entity.setRequestedStartDate(dto.getRequestedStartDate());
-        entity.setRequestedCompletionDate(dto.getRequestedCompletionDate());
-        entity.setExpectedCompletionDate(dto.getExpectedCompletionDate());
-        entity.setNotificationContact(dto.getNotificationContact());
-        entity.setBaseType(dto.getBaseType());
-        entity.setType(dto.getType());
-        entity.setSchemaLocation(dto.getSchemaLocation());
-        entity.setChannel(
-                entity.getChannel()
-                        .href(dto.getChannel().getHref())
-                        .name(dto.getChannel().getName())
-                        .role(dto.getChannel().getRole())
-                        .type(dto.getChannel().getType())
-        );
-        entity.setNote(dto.getNote());
-        entity.setBillingAccount(
-                entity.getBillingAccount()
-                        .href(dto.getBillingAccount().getHref())
-                        .name(dto.getBillingAccount().getName())
-                        .type(dto.getBillingAccount().getType())
-        );
+        if (dto.getHref() != null) {
+            entity.setHref(dto.getHref());
+        }
+        if (dto.getExternalId() != null) {
+            entity.setExternalId(dto.getExternalId());
+        }
+        if (dto.getPriority() != null) {
+            entity.setPriority(dto.getPriority());
+        }
+        if (dto.getDescription() != null) {
+            entity.setDescription(dto.getDescription());
+        }
+        if (dto.getCategory() != null) {
+            entity.setCategory(dto.getCategory());
+        }
+        if (dto.getState() != null) {
+            entity.setState(dto.getState());
+        }
+        if (dto.getOrderDate() != null) {
+            entity.setOrderDate(dto.getOrderDate());
+        }
+        if (dto.getCompletionDate() != null) {
+            entity.setCompletionDate(dto.getCompletionDate());
+        }
+        if (dto.getRequestedStartDate() != null) {
+            entity.setRequestedStartDate(dto.getRequestedStartDate());
+        }
+        if (dto.getRequestedCompletionDate() != null) {
+            entity.setRequestedCompletionDate(dto.getRequestedCompletionDate());
+        }
+        if (dto.getExpectedCompletionDate() != null) {
+            entity.setExpectedCompletionDate(dto.getExpectedCompletionDate());
+        }
+        if (dto.getNotificationContact() != null) {
+            entity.setNotificationContact(dto.getNotificationContact());
+        }
+        if (dto.getBaseType() != null) {
+            entity.setBaseType(dto.getBaseType());
+        }
+        if (dto.getType() != null) {
+            entity.setType(dto.getType());
+        }
+        if (dto.getSchemaLocation() != null) {
+            entity.setSchemaLocation(dto.getSchemaLocation());
+        }
+        if (dto.getChannel() != null) {
+            entity.setChannel(
+                    entity.getChannel()
+                            .href(dto.getChannel().getHref())
+                            .name(dto.getChannel().getName())
+                            .role(dto.getChannel().getRole())
+                            .type(dto.getChannel().getType())
+            );
+        }
+        if (dto.getNote() != null) {
+            entity.setNote(dto.getNote());
+        }
+        if (dto.getBillingAccount() != null) {
+            entity.setBillingAccount(
+                    entity.getBillingAccount()
+                            .href(dto.getBillingAccount().getHref())
+                            .name(dto.getBillingAccount().getName())
+                            .type(dto.getBillingAccount().getType())
+            );
+        }
         entity.setRelatedParty(List.of(
                 entity.getRelatedParty().get(0)
                         .href(dto.getRelatedParty().get(0).getHref())
